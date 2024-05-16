a0a7c95d858a711a9e30596557b6850ae67c24f5
MadamSagas <141463441+MadamSagas@users.noreply.github.com>
2024-05-16 22:40:11 +0000

    agregue el codigo necesario en el file models.py agegue la imagen QuickDBD-StarwarsBlog.png de quickdatabasediagrams

--- src/models.py
+++ src/models.py
@@ -4,26 +4,89 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
+from sqlalchemy import Float
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
+    # nullable=False
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(50))
+    email = Column(String(50), unique=True)
+    password = Column(String(32))
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class Characters(Base):
+    __tablename__ = 'characters'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    name = Column(String(50))   
+    last_name = Column(String(50))
+    height = Column(Float)
+    mass = Column(Integer)
+    birht_year = Column(String(50))
+    gender = Column(String(20))
+
+class Planets(Base):
+    __tablename__= 'planets'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    rotation_period = Column(Integer)
+    orbital_period = Column(Integer)
+    diameter = Column(Integer)
+    climate = Column(String(50))
+    terrain = Column(String(50))
+
+class Starships(Base):
+    __tablename__= 'starships'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50))
+    last_name = Column(String(50))
+    model = Column(String(50))
+    manufacturer = Column(String(50))
+    length = Column(Integer)
+    crew = Column(Integer)
+    passengers = Column(Integer)
+    consumables = Column(String(50))
+
+class FavoritesCharacters(Base):
+    __tablename__='favorites_characters'
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id')) 
+    user_id_relationship = relationship(User)
+    characters_id = Column(Integer, ForeignKey('characters.id'))
+    user_id_relationship = relationship(Characters)   
+
+
+class FavoritesPlanets(Base):
+    __tablename__= "favorites_planets"
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id_relationship = relationship(User)
+    planets_id = Column(Integer, ForeignKey('planets.id'))
+    planets_id_relationship = relationship(Planets)
+
+class FavoriteStarships(Base):
+    __tablename__= "favorites_starships"    
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user_id_relationship = relationship(User)
+    starships_id = Column(Integer, ForeignKey('starships.id'))
+    starships_id_ralationship = relationship(Starships)
+    
+
+
+# class Address(Base):
+#     __tablename__ = 'address'
+#     # Here we define columns for the table address.
+#     # Notice that each column is also a normal Python instance attribute.
+#     id = Column(Integer, primary_key=True)
+#     street_name = Column(String(250))
+#     street_number = Column(String(250))
+#     post_code = Column(String(250), nullable=False)
+#     person_id = Column(Integer, ForeignKey('person.id'))
+#     person = relationship(Person)
 
     def to_dict(self):
         return {}
